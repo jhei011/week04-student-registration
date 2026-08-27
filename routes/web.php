500008e7bee315c069787a3a8638c25473502fb9
jhei011 <emjhei.vctr@gmail.com>
2026-08-27 15:12:35 +0800

    feat: register student routes for create, store, and show

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [StudentController::class, 'create'])->name('students.create');
+Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
